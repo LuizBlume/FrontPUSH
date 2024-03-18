b6e81477921db5585fd35b6394e223cf1bce16dd
LuizBlume <luiz.fernando.251107@gmail.com>
2024-03-18 15:52:34 -0300

    Hotfix: Alterações de estilização no header.js.

--- React/blog/src/components/Header/index.js
+++ React/blog/src/components/Header/index.js
@@ -1,13 +1,20 @@
 import logosvg from '../../svg/logo.svg';
 const Header = () => {
+    const descLogo = "Essa é a descrição da logo"
     return (
         <>
-            <header>
-                <div className="logo border">
-                    <img src={logosvg} alt="logo" />
+            <header className="flex-space-between">
+                <div className="logo">
+                    <img src={logosvg} alt={descLogo} />
                 </div>
-                <div className="search"></div>
-                <div className="menu"></div>
+                <div className="search">
+                    <input type="text" class="input-search" name="search" placeholder="Pesquisar" />
+                </div>
+                <ul className="menu">
+                <li><a href="#" className="nav-link">Categories</a></li>
+                <li><a href="#" className="nav-link">About</a></li>
+                <li><a href="#" className="nav-link">Contact</a></li>
+                </ul>
             </header>
         </>
     );
