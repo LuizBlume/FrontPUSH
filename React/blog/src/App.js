0d1f6e24331a6702b4c8203983a69d2932ff437a
LuizBlume <luiz.fernando.251107@gmail.com>
2024-03-28 23:16:22 -0300

    fix: Alterações no código e uso de API JSON.

--- React/blog/src/App.js
+++ React/blog/src/App.js
@@ -2,10 +2,16 @@ import Header from './components/Header';
 import Article from './components/Article';
 
 function App() {
+  let carrinho = 0;
   return (
     <>
       <Header />
       <Article />
+      <h1>Carrinho de compras: {carrinho}</h1>
+      <div className="flex">
+        <button className="ml-2 add-button">+</button>
+        <button className="ml-2 remove-button">-</button>
+      </div>
     </>
   );
 }
