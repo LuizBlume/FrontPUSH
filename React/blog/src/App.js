3d03d03a2a51b7505b0bfb7ccf16dc81d931945e
LuizBlume <luiz.fernando.251107@gmail.com>
2024-03-30 23:40:18 -0300

    Fix: Uso de React Hooks.

--- React/blog/src/App.js
+++ React/blog/src/App.js
@@ -1,16 +1,26 @@
 import Header from './components/Header';
 import Article from './components/Article';
-
+import {useState} from 'react';
 function App() {
-  let carrinho = 0;
+  let [carrinho, setCarrinho] = useState(0);
+  const add = () => {
+    setCarrinho(carrinho + 1);
+    console.log(carrinho);
+  }
+  const remove = () => {
+    if (carrinho > 0) {
+      setCarrinho(carrinho - 1);
+      console.log(carrinho);
+    }
+  }
   return (
     <>
       <Header />
       <Article />
       <h1>Carrinho de compras: {carrinho}</h1>
       <div className="flex">
-        <button className="ml-2 add-button">+</button>
-        <button className="ml-2 remove-button">-</button>
+        <button className="ml-2 add-button" onClick={add}>+</button>
+        <button className="ml-2 remove-button" onClick={remove}>-</button>
       </div>
     </>
   );
